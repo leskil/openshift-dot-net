a21efcdb07a3f95191eec5c3e595296d6278339f
Lasse Eskildsen <leskil99@gmail.com>
2019-11-14 22:53:37 +0100

    Initial commit

---
+++ src/TimeService/TimeResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TimeService
+{
+    public class TimeResult
+    {
+        public string ApplicationName { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public long ElapsedMs { get; set; }
+        public TimeResult Response { get; set; }
+    }
+}
